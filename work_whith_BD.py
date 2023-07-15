fa13c79ba7e4ae11af772528c72b7093830eabf3
Yaroslav Galiulin <yaroslav66@list.ru>
2023-07-15 14:12:53 +0500

    Написание функций для работы с БД. Создание БД и таблиц со столбцами Функция заполнения таблицы с названием компании и кол-ом найденых вакансий

--- work_whith_BD.py
+++ work_whith_BD.py
@@ -20,7 +20,7 @@ def create_database():
             CREATE TABLE company_info (
             company_id int PRIMARY KEY,
             company_name varchar(255),
-            number_of_vacancies int
+            count_vacancy int
         )""")
 
     with conn.cursor() as cur:
@@ -33,6 +33,36 @@ def create_database():
         url text)""")
     conn.commit()
     conn.close()
-    print('типа создали')
+    print('Базу и таблицы создали')
 
-create_database()
+
+def write_table(data_list, company_id):
+
+    # vac['name']
+    #     vac['salary']
+    #     vac['salary']['from']
+    #     vac['salary']['to']
+    # company_id
+    # vac['alternate_url']
+    # vac['items'][0]['employer']['name']
+
+    #corteg_for_table = (company_id, data_list['items'][0]['employer']['name'], data_list['found'])
+    #print(corteg_for_table)
+
+    conn = psycopg2.connect(host='localhost', database='database_hhru', user='postgres', password='qwerty', port=5432)  # Данные БД
+
+    cur = conn.cursor()  # Включение курсора
+    corteg_for_table = (company_id, data_list['items'][0]['employer']['name'], data_list['found'])
+    cur.execute('INSERT INTO company_info VALUES (%s, %s, %s)', corteg_for_table)  # Добавление списка кортежей в таблицы
+
+
+
+        # cur.executemany('INSERT INTO customers VALUES (%s, %s, %s)', list_from_customers_data[1:])  # Добавление списка кортежей в таблицы
+        # cur.executemany('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', list_from_orders_data[1:])  # Добавление списка кортежей в таблицы
+        #
+        # cur.execute("""SELECT company_name FROM customers WHERE company_name LIKE '%v%'""")
+
+    conn.commit()
+    cur.close()  # Закрываем курсор
+    conn.close()  # Закрываем подключение к БД
+
